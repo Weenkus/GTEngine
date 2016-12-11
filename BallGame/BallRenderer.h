121db0e27de1d225624a5fd28ca24a1be09c7631
Vinko <vinko.kodzoman@fer.hr>
2016-12-11 19:40:24 +0100

    Add ball game

---
+++ BallGame/BallRenderer.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <GTEngine/SpriteBatch.h>
+#include <GTEngine/GLSLProgram.h>
+#include <vector>
+#include <memory>
+#include "Ball.h"
+
+// Ball renderer interface
+class BallRenderer {
+public:
+    virtual void renderBalls(GTEngine::SpriteBatch& spriteBatch, const std::vector<Ball>& balls,
+                             const glm::mat4& projectionMatrix);
+protected:
+    std::unique_ptr<GTEngine::GLSLProgram> m_program = nullptr;
+};
+
+// Visualizes kinetic energy
+class MomentumBallRenderer : public BallRenderer {
+public:
+    virtual void renderBalls(GTEngine::SpriteBatch& spriteBatch, const std::vector<Ball>& balls,
+                             const glm::mat4& projectionMatrix) override;
+};
+
+// Visualizes positive X component of velocity, as well as position
+class VelocityBallRenderer : public BallRenderer {
+public:
+    VelocityBallRenderer(int screenWidth, int screenHeight);
+
+    virtual void renderBalls(GTEngine::SpriteBatch& spriteBatch, const std::vector<Ball>& balls,
+                             const glm::mat4& projectionMatrix) override;
+private:
+    int m_screenWidth;
+    int m_screenHeight;
+};
+
+// Trippy renderer!
+class TrippyBallRenderer : public BallRenderer {
+public:
+    TrippyBallRenderer(int screenWidth, int screenHeight);
+
+    virtual void renderBalls(GTEngine::SpriteBatch& spriteBatch, const std::vector<Ball>& balls,
+                             const glm::mat4& projectionMatrix) override;
+private:
+    int m_screenWidth;
+    int m_screenHeight;
+    float m_time = 0.0f;
+};
---
+++ BallGame/MainGame.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <GTEngine/Camera2D.h>
+#include <GTEngine/SpriteBatch.h>
+#include <GTEngine/InputManager.h>
+#include <GTEngine/Window.h>
+#include <GTEngine/GLSLProgram.h>
+#include <GTEngine/Timing.h>
+#include <GTEngine/SpriteFont.h>
+#include <memory>
+
+#include "BallController.h"
+#include "BallRenderer.h"
+#include "Grid.h"
+
+// TODO:
+// Visualize momentum with color
+// Visualize velocity with color
+// Visualize position with color
+
+enum class GameState { RUNNING, EXIT };
+
+const int CELL_SIZE = 12;
+
+class MainGame {
+public:
+    ~MainGame();
+    void run();
+
+
+private:
+    void init();
+    void initRenderers();
+    void initBalls();
+    void update(float deltaTime);
+    void draw();
+    void drawHud();
+    void processInput();
+
+    int m_screenWidth = 0;
+    int m_screenHeight = 0;
+
+    std::vector<Ball> m_balls; ///< All the balls
+    std::unique_ptr<Grid> m_grid; ///< Grid for spatial partitioning for collision
+
+    int m_currentRenderer = 0;
+    std::vector<std::unique_ptr<BallRenderer> > m_ballRenderers;
+
+    BallController m_ballController; ///< Controls balls
+
+	GTEngine::Window m_window; ///< The main window
+	GTEngine::SpriteBatch m_spriteBatch; ///< Renders all the balls
+    std::unique_ptr<GTEngine::SpriteFont> m_spriteFont; ///< For font rendering
+	GTEngine::Camera2D m_camera; ///< Renders the scene
+	GTEngine::InputManager m_inputManager; ///< Handles input
+	GTEngine::GLSLProgram m_textureProgram; ///< Shader for textures]
+
+	GTEngine::FpsLimiter m_fpsLimiter; ///< Limits and calculates fps
+    float m_fps = 0.0f;
+
+    GameState m_gameState = GameState::RUNNING; ///< The state of the game
+};
+
